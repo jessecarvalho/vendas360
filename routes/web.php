696101b7a5117d7a436e99db9a7ec8fb84edb1fe
jessecarvalho <jesseelias80@gmail.com>
2024-02-03 13:28:56 -0300

    [feat] Created routes for sellers and sales

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/vendedores', function () {
+    return view('sellers');
+})->middleware(['auth', 'verified'])->name('vendedores');
+
+Route::get('/vendas', function () {
+    return view('sales');
+})->middleware(['auth', 'verified'])->name('vendas');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
