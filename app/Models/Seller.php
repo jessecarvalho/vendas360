bbbeebb131f8d52372c5b1bbf14d92285602ca5c
jessecarvalho <jesseelias80@gmail.com>
2024-02-03 16:27:52 -0300

    [fix] Fixed models name

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Sellers extends Model
+class Seller extends Model
 {
     protected $fillable = [
         'name',
