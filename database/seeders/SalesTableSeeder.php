be4795297d51d128a4e502209f3d7b626929ff72
jessecarvalho <jesseelias80@gmail.com>
2024-02-04 17:33:41 -0300

    [feat] Created Seeder for sales table

---
+++ database/seeders/SalesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class SalesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        for ($i = 0; $i < 12; $i++) {
+
+            $seller = DB::table('sellers')->inRandomOrder()->first();
+
+            DB::table('sales')->insert([
+                'value' => rand(100, 1000),
+                'date' => now(),
+                'seller_id' => $seller->id,
+            ]);
+        }
+    }
+}
