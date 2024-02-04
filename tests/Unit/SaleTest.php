6d2c8db6c3f5d2100773a51de703a3c3461dd33d
jessecarvalho <jesseelias80@gmail.com>
2024-02-04 15:18:20 -0300

    [test] Created tests for sales functions

---
+++ tests/Unit/SaleTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Seller;
+use App\Services\SellerServices;
+use Tests\TestCase;
+use App\Services\SaleServices;
+use App\Models\Sale;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SaleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $sellerServices = new SellerServices();
+        $sellerData = [
+            "name" => "John Doe",
+            "email" => "john@gmail.com",
+        ];
+        $sellerServices->insert($sellerData);
+        $this->seller = Seller::where('email', 'john@gmail.com')->first();
+    }
+
+    public function test_insert_sale()
+    {
+        $saleServices = new SaleServices();
+        $data = [
+            "seller_id" => $this->seller->id,
+            "value" => 100,
+            "date" => "2024-01-01",
+        ];
+        $inserted = $saleServices->insert($data);
+        $this->assertTrue($inserted);
+    }
+
+    public function test_update_sale()
+    {
+        $saleServices = new SaleServices();
+        $data = [
+            "seller_id" => $this->seller->id,
+            "value" => 100,
+            "date" => "2024-01-01",
+        ];
+        $inserted = $saleServices->insert($data);
+        $this->assertTrue($inserted);
+        $sale = Sale::where('value', 100)->first();
+        $data = [
+            "seller_id" => $this->seller->id,
+            "value" => 200,
+            "date" => "2024-01-01",
+        ];
+        $updated = $saleServices->update($data, $sale->id);
+        $this->assertTrue($updated);
+    }
+
+    public function test_delete_sale()
+    {
+        $saleServices = new SaleServices();
+        $data = [
+            "seller_id" => $this->seller->id,
+            "value" => 100,
+            "date" => "2024-01-01",
+        ];
+        $inserted = $saleServices->insert($data);
+        $this->assertTrue($inserted);
+
+        $sale = Sale::where('value', 100)->first();
+        $this->assertNotNull($sale, 'Sale was not inserted correctly');
+
+        $deleted = $saleServices->delete($sale->id);
+        $this->assertTrue($deleted, 'Sale was not deleted correctly');
+
+        $sale = Sale::where('value', 100)->first();
+        $this->assertNull($sale, 'Sale was not deleted correctly');
+    }
+
+}
