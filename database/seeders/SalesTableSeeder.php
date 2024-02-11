a8b5a85ed86a257eee2de0abfdc2fe7fffcee318
jessecarvalho <jesseelias80@gmail.com>
2024-02-11 19:39:53 -0300

    [fix] Sales seed now use factory

--- database/seeders/SalesTableSeeder.php
+++ database/seeders/SalesTableSeeder.php
@@ -2,24 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sale;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class SalesTableSeeder extends Seeder
 {
     public function run()
     {
-        for ($i = 0; $i < 12; $i++) {
-
-            $seller = DB::table('sellers')->inRandomOrder()->first();
-
-            DB::table('sales')->insert([
-                'value' => rand(100, 1000),
-                'date' => now(),
-                'commission' => rand(10, 100),
-                'seller_id' => $seller->id,
-            ]);
-        }
+        Sale::factory()
+            ->count(15)
+            ->create();
     }
 }
