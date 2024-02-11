57e3c047fa18472a5f3a380320219b5b8b3717e4
jessecarvalho <jesseelias80@gmail.com>
2024-02-11 19:39:03 -0300

    [fix] Users seed now use factory

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -14,10 +15,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => 'teste@gmail.com',
-            'password' => Hash::make('password'),
-        ]);
+        User::factory()
+            ->count(2)
+            ->create();
     }
 }
